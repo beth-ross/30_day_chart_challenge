06b0df48b9cee05eac0ad8f7bc862c5223865d1d
beth-ross <betheross@proton.me>
2026-04-01 15:36:54 -0600

    day 1 final

--- day_1_2026.R
+++ day_1_2026.R
@@ -1,25 +1,26 @@
 #Day 1 - Part-to-whole
 
-library(treemap)
-library(d3treeR)
+library(tidyverse)
+library(readxl)
+library(usmap)
 
-#remove '&' from data
-nps_econ2 <- nps_econ_data |> 
-  mutate(NPS_unit_nm = str_replace(NPS_unit_nm, "&", ""))
 
-#treemap doesn't match in box size or legend colors = smoky or golden gate should be
-# bigger than blue ridge
-nps_econ2 |> filter(econ_output > 2000000)
+#make chloropleth showing nps employement/total employement per state
 
-treemap(nps_econ2,
-        index = "NPS_unit_nm",
-        vSize = "econ_output",
-        #fontsize.labels = 15,
-        #bg.labels = c("white"),
-        vColor = "econ_output",
-        type = "value",
-        #palette = "BuPu",
-        title = "Economic Output by NPS Unit",
-        title.legend = "Output in $000")
+data <- nps_state_econ_data |> 
+  left_join(bls_data) |> 
+  mutate(proportion_jobs = (jobs/(total_employment_thousands*1000))*100) |> 
+  mutate(prop_tot_nps = (jobs/sum(jobs))*100) |> 
+  rename(full = state)
 
-#inter <- d3tree(nps_tree, width = "200%", height = "600px", rootname = "test")
+state <- us_map(regions = "states")
+
+map_data <- state |> 
+  left_join(data) |> 
+  filter(full != ("Puerto Rico"))
+
+ggplot(map_data) +
+  geom_sf(aes(fill = proportion_jobs)) +
+  theme_bw() +
+  labs(fill = "Percent NPS jobs")
+
--- nps_data_2026.R
+++ nps_data_2026.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 library(here)
+library(janitor)
+library(tigris)
+library(readxl)
 
 project_root <- here::here()
 
@@ -7,5 +10,13 @@ ni_data_folder <- "C:/Users/Beth/Documents/Biz/Next Interior/interior_contributi
 
 nps_econ_data <- readRDS(paste0(ni_data_folder,"NPS_econ_by_CD_2025-11-03.rds"))
 
+nps_state_econ_data_dirt <- read_xlsx("nps_state_econ_data.xlsx")
 
-                         
+bls_data_dirt <- read_xlsx("bls_state_data.xlsx")                         
+
+nps_state_econ_data <- clean_names(nps_state_econ_data_dirt)
+
+bls_data <- clean_names(bls_data_dirt)
+
+
+
